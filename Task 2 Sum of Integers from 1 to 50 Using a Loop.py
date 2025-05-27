4569d30a365f1b9754dc9acc758930817a40da48
behera-abhisek <behera.abhisek33@gmail.com>
2025-05-27 18:14:21 +0530

    Update Task 2 Sum of Integers from 1 to 50 Using a Loop.py

--- Task 2 Sum of Integers from 1 to 50 Using a Loop.py
+++ Task 2 Sum of Integers from 1 to 50 Using a Loop.py
@@ -1,3 +1,12 @@
+# Simple version to calculate sum of intergers from 1 to 50 using for loop
+t=0
+for n in range(1,51):
+    t+= n
+print("The sum of the numbers from 1 to 50 is",t)
+
+
+# Advanced version with user input method to calculate sum of integers in a given range
+"""
 start_num = int(input("Enter the starting number: "))
 end_num = int(input("Enter the ending number: "))
 total = 0
@@ -5,3 +14,6 @@ for number in range(start_num,end_num+1):
     #total = total + number
     total += number
 print("The sum of the numbers from", start_num, "to", end_num, "is", total)
+"""
+
+
